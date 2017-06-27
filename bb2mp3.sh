2b9f7f5aa262b1b8185596c6f5cc30c601ff13c1
ZweiSpeedruns <zcm727@gmail.com>
2017-06-27 06:47:07 -0500

    Increased quality
    turns out 16k is excessively low, most common bitrates are 128k and up
    
    Signed-off-by: ZweiSpeedruns <zcm727@gmail.com>

--- bb2mp3.sh
+++ bb2mp3.sh
@@ -5,5 +5,5 @@ read -p "Enter time in seconds: " T
 S=$[T*8000]
 echo $S
 echo "int main(){for(int t=0;;t++){putchar($BB);}}"|gcc -xc -
-./a.out|head -c $S|ffmpeg -f u8 -ar 8000 -i pipe: -b:a 16k o.mp3
+./a.out|head -c $S|ffmpeg -f u8 -ar 8000 -i pipe: -b:a 128k o.mp3
 rm a.out
